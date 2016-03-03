03d61c32deed08b3e1999db6f060dc4190a37985
Christopher Su <christophersu9@gmail.com>
2016-03-02 16:25:08 -0800

    animating self looped packets

--- netviz.js
+++ netviz.js
@@ -1,5 +1,15 @@
 (function() {
-  var graph;
+  // Returns an attrTween for translating along the specified path element.
+  function translateAlong(path) {
+    var l = path.getTotalLength();
+    return function(d, i, a) {
+      return function(t) {
+        var p = path.getPointAtLength(t * l);
+        return "translate(" + p.x + "," + p.y + ")";
+      };
+    };
+  }
+
   function Graph() {
     var existingNodes = {};
 
@@ -50,6 +60,42 @@
     };
 
     this.addPacket = function(source, target, color) {
+      // handle self loop
+      if (source === target) {
+        var n1 = findEdge(source);
+
+        var points = [
+          [n1.x, n1.y],
+          [n1.x+50, n1.y],
+          [n1.x+50, n1.y+50],
+          [n1.x, n1.y+50],
+          // [n1.x, n1.y],
+        ];
+
+        var path = svg.append("path")
+            .data([points])
+            .attr("d", d3.svg.line()
+            .tension(0) // Catmull–Rom
+            .interpolate("cardinal-closed"));
+
+        var packet = svg.append("rect")
+            .attr("x", n1.x)
+            .attr("y", n1.y)
+            .attr("width", 7)
+            .attr("height", 7)
+            .style("fill", color);
+        packet.transition()
+            .attrTween("transform", translateAlong(path.node()))
+            .ease("quad")
+            .duration(1500)
+            .each("end", function() {
+              d3.select(this).remove();
+              path.remove();
+            });
+
+        return;
+      }
+
       var n1 = findEdge(source);
       var n2 = findEdge(target);
 
@@ -63,7 +109,7 @@
           .attr("x", n2.x)
           .attr("y", n2.y)
           .ease("quad")
-          .duration(2000)
+          .duration(1500)
           .each("end", function() {
             d3.select(this).remove();
           });
@@ -155,18 +201,10 @@
           return "translate(" + d.x + "," + d.y + ")";
         });
 
-        edge.attr("x1", function(d) {
-            return d.source.x;
-        })
-            .attr("y1", function(d) {
-            return d.source.y;
-        })
-            .attr("x2", function(d) {
-            return d.target.x;
-        })
-            .attr("y2", function(d) {
-            return d.target.y;
-        });
+        edge.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
       });
 
       force
@@ -181,34 +219,34 @@
     updateGraph();
   }
 
-  function drawGraph() {
-    graph = new Graph("#network");
-
-    graph.addNode('Router 1');
-    graph.addNode('Router 11');
-    graph.addEdge('Router 1', 'Router 11', '15');
-    reorderNodes();
-  }
-
-  drawGraph();
-
-  function reorderNodes() {
+  function redrawNodes() {
     $(".node").each(function( index ) {
       var gnode = this.parentNode;
       gnode.parentNode.appendChild(gnode);
     });
   }
 
-  function redraw() {
-    d3.select("svg").remove();
-    drawGraph();
+  function drawNetwork() {
+    var socket = io("http://log.461.dev.christopher.su");
+    var graph = new Graph("#network");
+
+    socket.on('event', function(entry) {
+      console.log(entry);
+      // draw the node, if it doesn't exist already
+      graph.addNode("" + entry.router);
+
+      // if it's a packet being sent
+      if (entry.data.event === 'sending') {
+        // add the destination node if it doesn't exist
+        graph.addNode("" + entry.data.to);
+
+        // draw the packet for the cell type
+        graph.addPacket("" + entry.router, "" + entry.data.to, "blue");
+      }
+
+      redrawNodes();
+    });
   }
-})();
 
-(function() {
-  var socket = io("http://log.461.dev.christopher.su");
-
-  socket.on('event', function(entry) {
-    console.log('hi');
-  });
+  drawNetwork();
 })();
