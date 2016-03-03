d3f38de02e118de0fadc100851a3c74c46183570
Christopher Su <christophersu9@gmail.com>
2016-03-02 17:36:37 -0800

    kind of works now

--- netviz.js
+++ netviz.js
@@ -17,6 +17,8 @@
 
   var defaultColor = "black";
 
+  var animationDuration = 1500;
+
   // Returns an attrTween for translating along the specified path element.
   function translateAlong(path) {
     var l = path.getTotalLength();
@@ -31,6 +33,9 @@
   function Graph() {
     var existingNodes = {};
     var existingEdges = {};
+
+    var animationQueue = [];
+    var animationTimer = null;
 
     this.addNode = function(id) {
       if (id in existingNodes) {
@@ -48,7 +53,7 @@
       }
 
       var i = 0;
-      var n = findEdge(id);
+      var n = findNode(id);
       while (i < edges.length) {
         if ((edges[i]['source'] == n) || (edges[i]['target'] == n)) {
           edges.splice(i, 1);
@@ -56,7 +61,7 @@
         else i++;
       }
 
-      nodes.splice(findEdgeIndex(id), 1);
+      nodes.splice(findNodeIndex(id), 1);
       delete existingNodes[id];
       delete existingEdges[id];
       updateGraph();
@@ -65,8 +70,10 @@
     this.removeEdge = function(source, target) {
       if (!(source in existingNodes)) {
         var idx = existingEdges[source].indexOf(target);
-        if (index > -1) {
+        if (idx > -1) {
           existingEdges[source].splice(idx, 1);
+          idx = existingEdges[target].indexOf(source);
+          existingEdges[target].splice(idx, 1);
 
           for (var i = 0; i < edges.length; i++) {
             if (edges[i].source.id == source && edges[i].target.id == target) {
@@ -80,33 +87,60 @@
     };
 
     this.addEdge = function(source, target, value) {
+      console.log("adding: " + source + ", " + target);
+      console.log(existingNodes);
+      console.log(existingEdges);
       if (!(source in existingNodes) || !(target in existingNodes)) {
         return;
       }
 
       // check if edge already exists
       var idx = existingEdges[source].indexOf(target);
-      if (index > -1) {
+      if (idx > -1) {
         return;
       }
 
       existingEdges[source].push(target);
+      existingEdges[target].push(source);
 
       edges.push({
-        "source": findEdge(source),
-        "target": findEdge(target),
+        "source": findNode(source),
+        "target": findNode(target),
         "value": value});
+
+      // console.log("edge added: " + source + ", " + target);
 
       updateGraph();
     };
 
+    var processAnimation = function() {
+      var next = animationQueue.shift();
+      animatePacket(next.source, next.target, next.color);
+      if (animationQueue.length === 0) {
+        clearInterval(animationTimer);
+        animationTimer = null;
+      }
+    }
+
+    // throttle animations using a queue
     this.addPacket = function(source, target, color) {
+      animationQueue.push({
+        source: source,
+        target: target,
+        color: color,
+      });
+      if (animationTimer === null) {
+        animationTimer = setInterval(processAnimation, 200);
+      }
+    }
+
+    var animatePacket = function(source, target, color) {
       // handle self loop
       if (source === target) {
-        var n1 = findEdge(source);
+        var n1 = findNode(source);
 
         var points = [
-          [n1.x, n1.y],
+          // [n1.x, n1.y],
           [n1.x+50, n1.y],
           [n1.x+50, n1.y+50],
           [n1.x, n1.y+50],
@@ -128,7 +162,7 @@
         packet.transition()
             .attrTween("transform", translateAlong(path.node()))
             .ease("quad")
-            .duration(1500)
+            .duration(animationDuration)
             .each("end", function() {
               d3.select(this).remove();
               path.remove();
@@ -137,8 +171,8 @@
         return;
       }
 
-      var n1 = findEdge(source);
-      var n2 = findEdge(target);
+      var n1 = findNode(source);
+      var n2 = findNode(target);
 
       var packet = svg.append("rect")
           .attr("x", n1.x)
@@ -150,19 +184,19 @@
           .attr("x", n2.x)
           .attr("y", n2.y)
           .ease("quad")
-          .duration(1500)
+          .duration(animationDuration)
           .each("end", function() {
             d3.select(this).remove();
           });
     }
 
-    var findEdge = function(id) {
+    var findNode = function(id) {
       for (var i in nodes) {
         if (nodes[i]["id"] === id) return nodes[i];
       }
     };
 
-    var findEdgeIndex = function(id) {
+    var findNodeIndex = function(id) {
       for (var i = 0; i < nodes.length; i++) {
         if (nodes[i].id == id) {
           return i;
@@ -283,13 +317,16 @@
         // add the destination node if it doesn't exist
         var dest = "" + entry.data.to;
         graph.addNode(dest);
+
         graph.addEdge(source, dest, 20);
+        redrawNodes();
 
         // draw the packet for the cell type
-        graph.addPacket(source, dest, entry.data.cell_type in packetColors ? packetColors[entry.data.cell_type] : defaultColor);
+        var color = entry.data.cell_type in packetColors ? packetColors[entry.data.cell_type] : defaultColor;
+        // console.log("sending: " + source + ", " + dest + ", " + color);
+        graph.addPacket(source, dest, color);
+        redrawNodes();
       }
-
-      redrawNodes();
     });
   }
 
